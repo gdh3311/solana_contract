b8562b8feb6a6ab3851d56b72ba0a35bad10b780
gdh3311 <vycsnis1654@gmail.com>
2025-09-24 15:10:56 +0900

    testing example
    work on rib.rs actual inline code
    work on test for do test

--- programs/solana_contract/src/lib.rs
+++ programs/solana_contract/src/lib.rs
@@ -7,10 +7,34 @@ pub mod solana_contract {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        ctx.accounts.state.value = 0;
+        msg!("Initialized value: {}", ctx.accounts.state.value);
+        Ok(())
+    }
+
+    pub fn set_value(ctx: Context<SetValue>, new_value: u64) -> Result<()> {
+        ctx.accounts.state.value = new_value;
+        msg!("Updated value: {}", ctx.accounts.state.value);
         Ok(())
     }
 }
 
+#[account]
+pub struct StateAccount {
+    pub value: u64,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 16)]
+    pub state: Account<'info, StateAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct SetValue<'info> {
+    #[account(mut)]
+    pub state: Account<'info, StateAccount>,
+}
